48cbe0b79160969c77aff484ffa996bbac1fa794
vmgn16 <mg_619@hotmail.com>
2022-04-12 00:22:20 -0500

    ejercicio2

--- weekly_mission_2/exercises/ejercicio2/ejercicio2.js
+++ weekly_mission_2/exercises/ejercicio2/ejercicio2.js
@@ -58,13 +58,13 @@ const explorers = [
       }
     }
    ]
-
+console.log("Resolución de problemas\n")
 //1. Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH
-console.log("Nombre de los explorers: ")
+console.log("1. Nombre de los explorers: ")
 explorers.forEach(element => console.log(element.name));
 console.log("")
 //2. Imprime el stack de cada explorer, usa FOR EACH
-console.log("Stack de cada explorer: ")
+console.log("2. Stack de cada explorer: ")
 explorers.forEach(element => console.log(element.name + ": " + element.stack));
 console.log("")
 //3. Crea una nueva lista con las listas de stacks de cada explorer, usa MAP
@@ -74,7 +74,7 @@ const nueva_lista = explorers.map(Item => {
 
     return container;
 })
-console.log("Lista nueva de Stacks: ")
+console.log("3. Lista nueva de Stacks: ")
 console.log(nueva_lista)
 console.log("")
 //4. Obtén la lista de explorers que tengan en su stack "js", 
@@ -83,28 +83,27 @@ let search = (list, text) =>
 list.filter(i => i.stack.includes(text.toLowerCase()));
 
 let result = search(explorers, 'js');
-console.log("Explorers con Stack JS")
+console.log("4. Explorers con Stack JS")
 console.log(result)
 console.log("")
 //5. Busca el primer explorer que sea de la CDMX, usa FIND
 let buscar = explorers.find(tree => tree.city === "CDMX");
-console.log("Explorers de la CDMX")
+console.log("5. Explorers de la CDMX")
 console.log(buscar)
 console.log("")
 //6. Obtén la suma de todos los exercises_completed, usa REDUCE
 let suma = explorers.reduce((a,b) => ({exercises_completed: a.exercises_completed + b.exercises_completed})) ;
-console.log("Suma de todos los ejercicios completados por los Explorers")
+console.log("6. Suma de todos los ejercicios completados por los Explorers")
 console.log(suma);
 console.log("")
 //7. Obtén la validación si al menos uno de los explorers 
 //tiene la propiedad exercisesFinished en frontend como true, usa SOME
 const res = explorers.some(data => data.missions.frontend.exercisesFinished===true);
-console.log("Validación de al menos un explorer tiene true en exercisesFinished:")
+console.log("7. Validación de al menos un explorer tiene true en exercisesFinished:")
 console.log(res);
 console.log("");
 //8. Obtén la validación si todos los explorers 
 //tienen la propiedad isFinished del onboarding como true. Usa EVERY.
 const res1 = explorers.every(data => data.missions.onboarding.isFinished===true);
-console.log("Validación si todos los explorers tienen true en isFinished:")
+console.log("8. Validación si todos los explorers tienen true en isFinished:")
 console.log(res1);
-console.log("");
